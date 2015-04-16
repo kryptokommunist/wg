08a0e188fffc6d342237b3e9675ec8487998b19d
kryptokommunist <marcusding@me.com>
2015-04-17 01:04:52 +0200

    Fix Javascript

---
+++ app/assets/javascripts/jquery-mobile.js
@@ -0,0 +1,5 @@
+
+// for mobile ui
+//= require jquery
+//= require jquery_ujs
+//= require jquery.mobile
--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,5 +1,7 @@
 class StatusController < ApplicationController
 
+	#respond_to :html, :js
+
 	def show
 		@mates = Mate.all
 		@areas = Area.all
