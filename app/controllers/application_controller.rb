629ab3b77fa2eb7f7d844da900df38221ec67bbe
kryptokommunist <marcusding@me.com>
2015-04-19 05:13:07 +0200

    Add tablet funcionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :force_tablet_to_mobile
 
   has_mobile_fu
-
-  
-private
-	def force_tablet_to_mobile
-		
-	    session[:tablet_view] = false
-	    session[:mobile_view] = true
-
-
-	end
 
 end
