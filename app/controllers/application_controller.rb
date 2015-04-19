d39ad8cb6805e34762127cf6e58efa4d3d8d17ec
kryptokommunist <marcusding@me.com>
2015-04-19 04:45:44 +0200

    Improve switches

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :force_tablet_to_mobile
 
   has_mobile_fu
+
+def force_tablet_html
+	if is_tablet_device?
+    session[:tablet_view] = false
+    session[:mobile_view] = true
+	end
+end
 
 end
