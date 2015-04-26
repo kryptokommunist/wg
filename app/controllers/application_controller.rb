79ecacb87dab7f473d7b2e723c9b4295f0ca7076
kryptokommunist <marcusding@me.com>
2015-04-26 21:28:16 +0200

    Add reminder function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,16 +8,15 @@ class ApplicationController < ActionController::Base
   has_mobile_fu
 
 def remind_of_duties(mates)
-    Thread.new do
-  mates.each do |mate|
-    if mate.current_duty.due_to <= Time.zone.now + 5.hours && mate.balance != 1  # if due_to is 24:00, get's send at 21:00
+
+    mates.each do |mate|
+    if mate.current_duty.due_to <= Time.zone.now + 5.hours # if due_to is 24:00, get's send at 21:00
       message = "Hi #{mate.first_name},\ndeine noch offene Aufgabe: #{mate.current_duty.area.name}\nBitte erledige sie, bzw. trage die Erledigung ein!\nLink: #{root_url + "##{mate.first_name.downcase}"}\nDanke!\n\nSauberkeit: Wer reinigt, entfernt nichts, sondern verteilt nur anders."
       send_message(mate.mobile_number, message)
       mate.update_attribute(:balance, 1) # workaround for marking that user was reminded
     end
   end
-      ActiveRecord::Base.connection.close
-  end
+      
 end
 
 # sends message via Twilio
