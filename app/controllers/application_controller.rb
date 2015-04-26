9dfd223e7d9a2330945b30edda4683e7069ba587
kryptokommunist <marcusding@me.com>
2015-04-26 21:16:24 +0200

    Add reminder function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,10 @@ class ApplicationController < ActionController::Base
 
 def remind_of_duties(mates)
   mates.each do |mate|
-    if mate.current_duty.due_to <= Time.zone.now + 5.hours && mate.current_duty.accomplished_at.nil?  # if due_to is 24:00, get's send at 21:00
+    if mate.current_duty.due_to <= Time.zone.now + 5.hours && mate.balance != 1  # if due_to is 24:00, get's send at 21:00
       message = "Hi #{mate.first_name},\ndeine noch offene Aufgabe: #{mate.current_duty.area.name}\nBitte erledige sie, bzw. trage die Erledigung ein!\nLink: #{root_url + "##{mate.first_name.downcase}"}\nDanke!\n\nSauberkeit: Wer reinigt, entfernt nichts, sondern verteilt nur anders."
       send_message(mate.mobile_number, message)
-      mate.current_duty.update_attribute(:faulty, true)
+      mate.update_attribute(:balance, 1)
     end
   end
 end
