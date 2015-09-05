2e83a0ac3077d89ae00f57fa3311f755f579ab28
kryptokommunist <marcusding@me.com>
2015-09-05 15:01:46 +0200

    Test webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -3,7 +3,7 @@ class WebhooksController < ApplicationController
   def get_updates
 
     chat_id = params[:message][:from][:id] #get chat id from last received message
-    user = params[:message][:from][:text] #get user
+    user = params[:message][:text] #get user
 
     if mate = Mate.find_by(last_name: user) then
       mate.update_attribute(:chat_id)
