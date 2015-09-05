dd88a81bfd3d9e05d3c9454c542c70f1c79707e7
kryptokommunist <marcusding@me.com>
2015-09-05 13:17:36 +0200

    Test webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,9 @@
 class WebhooksController < ApplicationController
 
+  def handle_unverified_request
+      forgery_protection_strategy.new(self).handle_unverified_request
+  end
+
   def get_updates
 
     render :layout => false
