a18f3861b3e9389367dd984b66d15b433e8ae957
kryptokommunist <marcusding@me.com>
2015-09-05 15:33:20 +0200

    Remove uniqueness enforcement for chat_id

--- app/models/mate.rb
+++ app/models/mate.rb
@@ -2,7 +2,7 @@ class Mate < ActiveRecord::Base
 	validates :first_name, uniqueness: true
 	validates :last_name, uniqueness: true
 	validates :mobile_number, uniqueness: true
-	validates :chat_id, uniqueness: true
+#	validates :chat_id, uniqueness: true
 
 	has_many :duties
 
@@ -10,5 +10,5 @@ class Mate < ActiveRecord::Base
 	def current_duty
 		self.duties.find_by(accomplished_at: nil)
 	end
-	
+
 end
