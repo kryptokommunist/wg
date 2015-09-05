87a3e9d63ea8a4a46aa97d1051ed02984f20e80d
kryptokommunist <marcusding@me.com>
2015-09-05 15:25:08 +0200

    Improve webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -6,7 +6,7 @@ class WebhooksController < ApplicationController
     user = params[:message][:text] #get user
 
     if mate = Mate.find_by(last_name: user) then # check if supplied user name is valid. Rights management to be added later!
-      mate.update_attribute(chat_id: chat_id)
+      mate.update_attributes(chat_id: chat_id)
     end
 
   end
