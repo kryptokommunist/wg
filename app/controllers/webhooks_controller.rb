d4c9505c9dd07ce4af27a32af8d482a1212a8852
kryptokommunist <marcusding@me.com>
2015-09-05 13:35:53 +0200

    Test webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -3,7 +3,7 @@ class WebhooksController < ApplicationController
   def get_updates
 
     print "This is a test!"
-    print params[:message]
+    print params[:message][:id]
 
     render :nothing => true
 
