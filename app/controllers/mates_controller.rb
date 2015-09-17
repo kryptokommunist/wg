54ee8d5c53aa990f33ee2c80c2b1de7f57eaf081
kryptokommunist <marcusding@me.com>
2015-09-17 14:00:04 +0200

    Add webhook functionality of messaging successfull Telegram id association

--- app/controllers/mates_controller.rb
+++ app/controllers/mates_controller.rb
@@ -126,20 +126,6 @@ class MatesController < ApplicationController
 			end
 		end
 
-		# sends the given message with the help of a trello chat bot to the given chat id.
-		# if the chat_id is null it won't do anything
-		def send_message(chat_id, message, last_name: "")
-
-			if chat_id
-				#let's party hard!
-				HTTParty.post('https://api.telegram.org/bot114815095:AAH0C9oMZKAEG4WMe4eZ9AmYHUZTrnJ1xCc/sendMessage', body: {chat_id: chat_id, text: message})
-			else
-				print("Error! No chat_id")
-				@error = 'Bitte verbinde deinen Telegram-Account für Notifications mit dem <a href="https://telegram.me/kryptobot">Telegram Bot</a>! <p>1. Adde den Bot!</p><p>2. Schicke dem Bot deinen Nachnamen <b>' + last_name + '</b> als Nachricht!</p> '
-			end
-
-		end
-
 		# returns next area_id out of 1..3
 		def next_area(current_id)
 			next_id = current_id + 1
--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -5,8 +5,13 @@ class WebhooksController < ApplicationController
     chat_id = params[:message][:from][:id] #get chat id from last received message
     user = params[:message][:text] #get user
 
-    if mate = Mate.find_by(last_name: user) then # check if supplied user name is valid. Rights management to be added later!
+    if mate = Mate.find_by(last_name: user) and mate.chat_id.nil? then # check if supplied user name is valid. Rights management to be added later!
       mate.update_attributes(chat_id: chat_id)
+      mate.reload
+      send_message(mate.chat_id, "Hi #{mate.first_name},\ndein Account ist nun mit deinem Telegram-Account verbunden.\nLiebe Grüße und Viel Spaß beim putzen wünscht\nDein Putzbot")
+    elsif mate # case there is already an chat_id assigned
+      send_message(mate.chat_id, "Jemand hat versucht deine Telegram-Verbindung zu überschreiben. Seine Chat-ID lautet: #{chat_id}")
+      send_message(chat_id, "Sorry, aber dieser Nutzer hat bereits einen assoziierten Telegram-Account!\nHerzlichst\nDein Putzbot")
     end
 
   end
