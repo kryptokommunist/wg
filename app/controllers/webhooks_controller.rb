07f2db1340e5be0e50d9afeeebb8d2abf5f77121
kryptokommunist <marcusding@me.com>
2015-09-17 14:18:49 +0200

    Add responsiveness of Telegram bot in webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -5,13 +5,18 @@ class WebhooksController < ApplicationController
     chat_id = params[:message][:from][:id] #get chat id from last received message
     user = params[:message][:text] #get user
 
-    if mate = Mate.find_by(last_name: user) and mate.chat_id.nil? then # check if supplied user name is valid. Rights management to be added later!
-      mate.update_attributes(chat_id: chat_id)
-      mate.reload
-      send_message(mate.chat_id, "Hi #{mate.first_name},\ndein Account ist nun mit deinem Telegram-Account verbunden.\nLiebe Grüße und Viel Spaß beim putzen wünscht\nDein Putzbot")
+      if mate = Mate.find_by(last_name: user) and mate.chat_id.nil? then # check if supplied user name is valid. Rights management to be added later!
+        begin
+          mate.update_attributes!(chat_id: chat_id)
+        rescue ActiveRecord::RecordInvalid => e
+          send_message(chat_id, "Leider gab es Fehler o_O :\n\n#{e.to_s}")
+        end
+
     elsif mate # case there is already an chat_id assigned
-      send_message(mate.chat_id, "Jemand hat versucht deine Telegram-Verbindung zu überschreiben. Seine Chat-ID lautet: #{chat_id}")
-      send_message(chat_id, "Sorry, aber dieser Nutzer hat bereits einen assoziierten Telegram-Account!\nHerzlichst\nDein Putzbot")
+      send_message(mate.chat_id, "Hi #{mate.first_name},\njemand hat versucht deine Telegram-Verbindung zu überschreiben. Seine Chat-ID lautet: #{chat_id}")
+      send_message(chat_id, "Sorry, aber dieser Nutzer hat bereits einen assoziierten Telegram-Account!\n\nHerzlichst\nDein Putzbot")
+    else
+      send_message(chat_id, "Was willst du mir sagen? Ich verstehe nicht...\nSprich doch mal mit meinem Papa Hans :)\n\nHerzlichst\nDein Putzbot")
     end
 
   end
